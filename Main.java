70c8a800fe64064b5077928ef7716c4576a39723
ahmetsn34 <ahmettosun.dev@gmail.com>
2025-03-03 23:05:42 +0300

    Boy Kütle Endeksini hesaplayan bir kod bloğu yazdım çok tehlikeli oldu (!) :)

--- Main.java
+++ Main.java
@@ -1,6 +1,18 @@
-public class Main {
+import java.util.Scanner;
+
+public class Main 
+  {
     public static void main(String[] args)
     {
-      System.out.println("Hello World");
+      Scanner scanner = new Scanner(System.in); //bir sistem tarayıcısı oluşturdum 
+      System.out.print("Boyunu Gir (cm) : "); //boy bilgisi için bilgilendirme metni yazdırdım 
+      double x = scanner.nextDouble(); // tarayıcıya bunu sayır verisi olarak alması için komut verdim
+
+      System.out.print("Kilonu Gir (kg) : "); //üsteki işlemlerin aynısını kilo için yaptırıyorum 
+      double y = scanner.nextDouble(); // ""
+
+      x=x/100;
+      double endeks = y/(x*x); //boy kilo endeksinin matematiksle olarak hesaplamasını yaptırdım
+       System.out.println(endeks); // ve cıktısını veren komutu yazdırdım . 
     }
   }
